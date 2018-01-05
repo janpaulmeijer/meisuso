8df661b503a0e51c537367e7c292f91ae14a343c
Jan-Paul <janpaulmeijer@gmail.com>
2018-01-05 20:36:41 +0100

    Password change form

--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from accounts.forms import RegistrationForm
-from django.contrib.auth.forms import UserChangeForm
+from accounts.forms import (
+    RegistrationForm,
+    EditProfileForm
+)
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 
 def home(request):
     numbers = [1,2,3,4,5]
@@ -27,12 +30,24 @@ def view_profile(request):
 
 def edit_profile(request):
     if request.method == 'POST':
-        form = UserChangeForm(request.POST, instance=request.user)
+        form = EditProfileForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
             return redirect('/account/profile')
     else:
-        form = UserChangeForm(instance=request.user)
+        form = EditProfileForm(instance=request.user)
 
         args = {'form': form}
         return render(request, 'accounts/edit_profile.html', args)
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('/account/profile')
+    else:
+        form = PasswordChangeForm(request.user)
+
+        args = {'form': form}
+        return render(request, 'accounts/change_password.html', args)
