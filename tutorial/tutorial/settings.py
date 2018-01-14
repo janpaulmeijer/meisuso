bb52072c74dfe8c085c0737fd319226c0fee7054
Jan-Paul <janpaulmeijer@gmail.com>
2018-01-14 17:23:47 +0100

    add jumbotron image and panes for products

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -119,5 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
